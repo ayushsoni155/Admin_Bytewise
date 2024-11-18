3e0a59675bb3c2d3fc073f120bbcc37a3f45f0bf
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-11-18 22:41:30 +0530

    Update Admindashboard.jsx

--- src/component/jsx/Admindashboard.jsx
+++ src/component/jsx/Admindashboard.jsx
@@ -18,7 +18,7 @@ const Admindashboard = () => {
   // Function to fetch dashboard data
 const fetchDashboardData = async (date) => {
   try {
-    let url = 'http://localhost:3000/dashboard-data';
+    let url = 'https://bytewise-server.vercel.app/api/dashboard-data';
     if (date) {
       url = `${url}?date=${date}`; // Append the selected date to the URL
     }
