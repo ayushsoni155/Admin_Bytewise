8f0680c218a1d030d285122254f9fe9a3be8a2d5
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-11-20 11:28:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Adminnavbar from './component/jsx/Adminnavbar';
 import Admindashboard from './component/jsx/Admindashboard';
 import AdminUser from './component/jsx/AdminUser';
 import AdminFeedback from './component/jsx/AdminFeedback';
-
+import AdminOrder from './component/jsx/AdminOrder';
 
 const App = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
         <Route path="/" element={<Admindashboard/>} />
         <Route path="/User" element={<AdminUser/>} />
         <Route path="/Feedback" element={<AdminFeedback/>} />
-       
+         <Route path="/Order" element={<AdminOrder/>} />
      </Routes>
     </Router>
   );
